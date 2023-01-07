e9c27c8fd686ae75d43708864d232fb7ff8a05b4
Olipolite <oliver.oquist3d@gmail.com>
2023-01-07 15:48:10 +0000

    Tests are now working properly

--- src/models/index.js
+++ src/models/index.js
@@ -1,22 +1,17 @@
 const Sequelize = require('sequelize');
-const ReaderModel = require('./reader');
 
 const { PGDATABASE, PGUSER, PGPASSWORD, PGHOST, PGPORT } = process.env;
 
 const setupDatabase = () => {
-  const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
+  const connection = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
     host: PGHOST,
     port: PGPORT,
     dialect: 'postgres',
     logging: false,
   });
 
-  const Reader = ReaderModel(connection, Sequelize);
-
   connection.sync({ alter: true });
-  return {
-    Reader
-  };
+  return {};
 };
 
 module.exports = setupDatabase();
--- src/models/reader.js
+++
@@ -1,9 +0,0 @@
-module.exports = (connection, DataTypes) => {
-    const schema = {
-      name: DataTypes.STRING,
-      email: DataTypes.STRING,
-    };
-  
-    const ReaderModel = connection.define('Reader', schema);
-    return ReaderModel;
-  };
