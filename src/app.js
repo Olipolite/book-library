a4a37859953cb08e2a40731fed53b97c7972eded
Olipolite <oliver.oquist3d@gmail.com>
2023-01-16 19:14:35 +0000

    trying to pass the rest of the tests for CRUD

--- src/app.js
+++ src/app.js
@@ -7,4 +7,8 @@ app.use(express.json());
 
 app.post('/readers', readerController.createReader);
 
+app.get('/readers', readerController.getReader);
+
+app.get('/readers/:id', readerController.getReaderId);
+
 module.exports = app;
--- src/controllers/readers.js
+++ src/controllers/readers.js
@@ -4,3 +4,22 @@ exports.createReader = async (req, res) => {
   const newReader = await Reader.create(req.body);
   res.status(201).json(newReader);
 };
+
+exports.getReader = async (req, res) => {
+  const readers = await Reader.findAll(req.body);
+  res.status(200).json(readers);
+};
+
+exports.getReaderId = async (req, res) => {
+  try {
+    const readerId = req.params.id;
+    const reader = await Reader.findByPk(readerId);
+    res.status(200).json(reader);
+
+    if(!readerId) {
+      return res.status(404).json({ message: 'The reader could not be found' });
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+}
